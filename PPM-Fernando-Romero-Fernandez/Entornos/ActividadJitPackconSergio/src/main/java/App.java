4bbb03d6e3078b0fb85c1d12b193baa2b8712a8d
Fernando Romero <fernandonerja00@gmail.com>
2020-04-01 21:26:40 +0200

    Actividad JitPack con Sergio

---
+++ PPM-Fernando-Romero-Fernandez/Entornos/ActividadJitPackconSergio/src/main/java/App.java
@@ -0,0 +1,10 @@
+import com.politecnicomalaga.Copyright;
+
+public class App {
+
+    public static void main(String[] args) {
+        Copyright copyright = new Copyright();
+        System.out.println(copyright.getText());
+    }
+
+}
